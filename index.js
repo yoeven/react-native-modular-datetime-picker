6695ce8177406cafc3089e3b48dd3074caa40429
Yoeven D Khemlani <yoevenkhem@gmail.com>
2021-01-15 20:32:33 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+import dayjs from "dayjs";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import utc from "dayjs/plugin/utc";
+import isBetween from "dayjs/plugin/isBetween";
+import dayOfYear from "dayjs/plugin/dayOfYear";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(localizedFormat);
+dayjs.extend(utc);
+dayjs.extend(isBetween);
+dayjs.extend(dayOfYear);
+dayjs.extend(weekOfYear);
+dayjs.extend(advancedFormat);
+
 import DatePicker from "./components/DatePicker";
 import TimePicker from "./components/TimePicker";
 import DateTimePicker from "./components/DateTimePicker";
